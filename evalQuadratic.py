10276015378677fa969b1c3ec2cd7c0aa7e9eb8c
SuyashD95 <suyashdayal@gmail.com>
2017-03-21 14:02:17 +0530

    Insert the question as a comment in evalQuadratic.py

--- evalQuadratic.py
+++ evalQuadratic.py
@@ -1,3 +1,7 @@
+"""
+Q2- Write a Python function, evalQuadratic(a, b, c, x), that returns the value of the quadratic  a*x2+b*x+c. This function takes in four numbers and returns a single number. 
+"""
+
 def evalQuadratic( a, b, c, x ):
 
 	a = int ( a )
