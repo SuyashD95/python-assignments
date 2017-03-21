5cf413353c62f127e40a65c582b3441675874398
SuyashD95 <suyashdayal@gmail.com>
2017-03-21 14:04:26 +0530

    Insert the question as a comment in odd.py for reader's reference

--- odd.py
+++ odd.py
@@ -1,3 +1,7 @@
+"""
+Q4- Write a Python function, odd, that takes in one number and returns True when the number is odd and False otherwise. You should use the % (mod) operator, not if. This function takes in one number and returns a boolean
+"""
+
 def odd( number ):
 
 	return number % 2 == 1
