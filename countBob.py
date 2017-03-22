c739315822a096798a688873f17e24d9f03b3d0a
SuyashD95 <suyashdayal@gmail.com>
2017-03-22 10:48:29 +0530

    Final version of countBob

--- countBob.py
+++ countBob.py
@@ -6,8 +6,22 @@ is: 2
 
 def countBob( string ):
 
-	
+	count = 0
+	start = 0
 
-countBob( "azcbobobegghakl" )
-print("End")
-#print( str( countBob( "azcbobobegghakl" ) ) )
+	while string.find( "bob" ) != -1:
+
+		start = string.find( "bob" ) # Finds the first character where the substring "bob" first appears.
+		
+		# Strings are immutable. Hence, we store the new string by slicing the existing string from position start + 1 to the last position(length of string - 1)
+		string = string[ start + 1 : ]
+		count += 1
+
+	return count
+
+print( "Remember all the characters in the string should be in LOWERCASE" )
+string = input( "Enter the string: ")
+print( "Number of times bob occurs is: " + str( countBob( string ) ) )
+
+# Uncomment the following line if you are using Console/Terminal
+# input("Press any key to exit..." )
