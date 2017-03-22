e543a6e12f34dfdde4f55630fcd1514d7622e0ee
SuyashD95 <suyashdayal@gmail.com>
2017-03-22 10:54:05 +0530

    Add the answer of seventh question of Assignment 3

--- countBob.py
+++ countBob.py
@@ -1,4 +1,5 @@
-""" Q7- Assume s is a string of lower case characters. Write a program that
+""" 
+Q7- Assume s is a string of lower case characters. Write a program that
 prints the number of times the string 'bob' occurs in s. For example, if s =
 'azcbobobegghakl', then your program should print Number of times bob occurs
 is: 2  
@@ -21,7 +22,5 @@ def countBob( string ):
 
 print( "Remember all the characters in the string should be in LOWERCASE" )
 string = input( "Enter the string: ")
-print( "Number of times bob occurs is: " + str( countBob( string ) ) )
 
-# Uncomment the following line if you are using Console/Terminal
-# input("Press any key to exit..." )
+print( "Number of times bob occurs is: " + str( countBob( string ) ) )
